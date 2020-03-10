5c0cbe5a2b7cdcb27b6989843e3fdcea3f639d96
Andrew McMechan <coderwurst@gmail.com>
2020-03-10 20:47:49 +0100

    core types

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('here I am!!');
+// core type number
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+console.log(add(number1, number2));
+// core type string
+function findWord(input, searchTerm) {
+    return input.indexOf(searchTerm) !== -1 ? 'word found' : 'word not found';
+}
+console.log(findWord('is this the end', 'is'));
+console.log(findWord('is this the end', 'no'));
+// core type boolean
+function isTrue(value) {
+    return value ? true : false;
+}
+console.log(isTrue(0));
+console.log(isTrue(NaN));
+console.log(isTrue(1));
--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-console.log('here I am!');
+// core type number
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+console.log(add(number1, number2));
+
+// core type string
+function findWord(input: string, searchTerm: string) {
+  return input.indexOf(searchTerm) !== -1 ? 'word found' : 'word not found';
+}
+
+console.log(findWord('is this the end', 'is'));
+console.log(findWord('is this the end', 'no'));
+
+// core type boolean
+function isTrue(value: boolean) {
+  return value ? true : false;
+}
+
+// type error examples
+console.log(isTrue(0));
+console.log(isTrue(NaN));
+console.log(isTrue(1));
